c9eeb2733188a424041c76604c72dd6a313f19c6
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-24 17:02:06 -0300

    dinamico

--- template-parts/content-home-banner-item.php
+++ template-parts/content-home-banner-item.php
@@ -9,26 +9,25 @@
                 <div class="row">
 
                     <!-- loop -->
-                    <div class="col-lg-4 my-3 my-lg-0">
+                     <div class="col-lg-4 my-3 my-lg-0">
                         <a 
                         class="card h-100 border-0 rounded-0 text-decoration-none u-bg-cover u-bg-no-repeat"
                         style="background-color:red"
-                        href="<?php echo get_field('link_aviso'); ?>">
+                        href="<?php echo get_field('link_esq'); ?>">
                             <div class="card-body d-flex flex-column justify-content-center align-items-center">
                                 
 
                                 <p class="u-font-size-18 xl:u-font-size-22 u-font-weight-semibold u-font-family-nunito text-center u-color-folk-bold-gray">
-                                   <?php echo get_field('descricao_aviso'); ?>     
+                                   <?php echo get_field('icone_esq'); ?>     
                             
                                 </p>
                                 <h3 class="u-font-size-42 xxl:u-font-size-52 u-font-weight-bold u-font-family-cinzel-decorative text-center u-color-folk-dark-marron mb-4">
-                                    Avisos <br>
-                                    Paroquiais:
+                                    <?php echo get_field('titulo_icon_esquerdo'); ?>
                                 </h3>
                                 <span 
                                 class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-regular u-font-family-lato text-center text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden py-2" 
-                                href="<?php echo get_home_url( null, '/fotos'); ?>">
-                                   Ver agora
+                                >
+                                <?php echo get_field('botao_esq'); ?>
                                 </span>
                             </div>
                         </a>
@@ -68,22 +67,21 @@
                         <a 
                         class="card h-100 border-0 rounded-0 text-decoration-none u-bg-cover u-bg-no-repeat"
                         style="background-color:red"
-                        href="<?php echo get_field('link_aviso'); ?>">
+                        href="<?php echo get_field('link_dir'); ?>">
                             <div class="card-body d-flex flex-column justify-content-center align-items-center">
                                 
 
                                 <p class="u-font-size-18 xl:u-font-size-22 u-font-weight-semibold u-font-family-nunito text-center u-color-folk-bold-gray">
-                                   <?php echo get_field('descricao_aviso'); ?>     
+                                   <?php echo get_field('icone_dir'); ?>     
                             
                                 </p>
                                 <h3 class="u-font-size-42 xxl:u-font-size-52 u-font-weight-bold u-font-family-cinzel-decorative text-center u-color-folk-dark-marron mb-4">
-                                    Avisos <br>
-                                    Paroquiais:
+                                    <?php echo get_field('titulo_icon_direito'); ?>
                                 </h3>
                                 <span 
                                 class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-regular u-font-family-lato text-center text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden py-2" 
-                                href="<?php echo get_home_url( null, '/fotos'); ?>">
-                                   Ver agora
+                                >
+                                <?php echo get_field('botao_dir'); ?>
                                 </span>
                             </div>
                         </a>
