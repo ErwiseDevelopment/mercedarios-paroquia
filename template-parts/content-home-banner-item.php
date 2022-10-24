c938c863632f95507ff7b871de5792a44c9ba874
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-24 17:15:41 -0300

    dinamico

--- template-parts/content-home-banner-item.php
+++ template-parts/content-home-banner-item.php
@@ -33,8 +33,8 @@
                         </a>
                     </div>
                     
-                    <div class="col-lg-4 my-3 my-lg-0">
-                    <?php
+                    <!-- <div class="col-lg-4 my-3 my-lg-0">
+                    <php
                         $args = array(
                             'posts_per_page' => 1,
                             'post_type'      => 'aviso',
@@ -48,8 +48,8 @@
                     ?>
                         <a 
                         class="card h-100 border-0 rounded-0 text-decoration-none u-bg-cover u-bg-no-repeat"
-                        style="background-image: url('<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/notification-background.png')"
-                        href="<?php echo get_field('link_aviso'); ?>">
+                        style="background-image: url('<php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/notification-background.png')"
+                        href="<php echo get_field('link_aviso'); ?>">
                             <div class="card-body d-flex flex-column justify-content-center align-items-center">
                                 <h3 class="u-font-size-42 xxl:u-font-size-52 u-font-weight-bold u-font-family-cinzel-decorative text-center u-color-folk-dark-marron mb-4">
                                     Avisos <br>
@@ -57,13 +57,13 @@
                                 </h3>
 
                                 <p class="u-font-size-18 xl:u-font-size-22 u-font-weight-semibold u-font-family-nunito text-center u-color-folk-bold-gray">
-                                   <?php echo get_field('descricao_aviso'); ?>     
+                                   ?php echo get_field('descricao_aviso'); ?>     
                             
                                 </p>
                             </div>
                         </a>
-                        <?php endwhile; endif;?> 
-                    </div>
+                        <php endwhile; endif;?> 
+                    </div> -->
                   
 
                     <div class="col-lg-4 my-3 my-lg-0">
