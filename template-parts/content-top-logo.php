7e5c5f6f1facad1455f85062f9862c9c1b8b5191
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-24 15:57:48 -0300

    dinamico

--- template-parts/content-top-logo.php
+++ template-parts/content-top-logo.php
@@ -11,14 +11,14 @@
                     <div class="col-lg-4 mb-4 mb-lg-0">
                         <a 
                         class="d-block text-center u-bg-folk-dark-marron py-3 px-2"
-                        href="">
+                        >
                             <span class="u-line-height-100 u-font-size-22 xxl:u-font-size-28 u-font-weight-bold u-font-family-cinzel u-color-folk-white"><?php echo get_field('nome_paroquia', 'option') ?></span> <br>
                             <span class="u-font-size-12 u-font-weight-bold u-font-family-lato u-color-folk-white"><?php echo get_field ('local_paroquia', 'option') ?></span>
                         </a>
                     </div>
 
                     <div class="col-10 col-lg-3">
-                        <a href="#">
+                        <a >
                             <img 
                             class="img-fluid w-100 u-object-fit-cover"
                             src="<?php echo get_field('logo', 'option')?>" 
