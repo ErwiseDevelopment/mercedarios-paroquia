078dd3750a5c5095039d306ae23673f4aa62dbd3
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-24 15:58:50 -0300

    dinamico

--- template-parts/content-top-logo.php
+++ template-parts/content-top-logo.php
@@ -10,7 +10,7 @@
 
                     <div class="col-lg-4 mb-4 mb-lg-0">
                         <a 
-                        class="d-block text-center u-bg-folk-dark-marron py-3 px-2"
+                        class="d-block text-center u-bg-folk-dark-marron py-3 px-2 text-decoration-none"
                         >
                             <span class="u-line-height-100 u-font-size-22 xxl:u-font-size-28 u-font-weight-bold u-font-family-cinzel u-color-folk-white"><?php echo get_field('nome_paroquia', 'option') ?></span> <br>
                             <span class="u-font-size-12 u-font-weight-bold u-font-family-lato u-color-folk-white"><?php echo get_field ('local_paroquia', 'option') ?></span>
