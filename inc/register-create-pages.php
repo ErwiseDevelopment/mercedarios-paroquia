2e1b9b1a50440f4f415ff9da7a5f5c4d3f04ab14
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-24 18:00:04 -0300

    dinamico

--- inc/register-create-pages.php
+++ inc/register-create-pages.php
@@ -35,7 +35,7 @@ function ed_create_page() {
                 'menu_title' => 'API', 
                 'menu_slug'  => 'api', 
                 'capability' => 'edit_posts', 
-                'position'   => '23,7', 
+                'position'   => '100', 
                 'redirect'   => false, 
                 'icon_url'   => 'dashicons-rest-api',
         ));
@@ -48,7 +48,7 @@ function ed_create_page() {
                 'menu_title' => 'Avisos', 
                 'menu_slug'  => 'Avisos', 
                 'capability' => 'edit_posts', 
-                'position'   => '10,7', 
+                'position'   => '20', 
                 'redirect'   => false, 
                 'icon_url'   => 'dashicons-open-folder',
         ));
