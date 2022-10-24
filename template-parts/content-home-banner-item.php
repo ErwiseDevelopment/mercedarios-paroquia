e9f966917d6b6f14ffbbbeb5e9427d1578362981
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-24 16:19:36 -0300

    dinamico

--- template-parts/content-home-banner-item.php
+++ template-parts/content-home-banner-item.php
@@ -25,11 +25,11 @@
                                     Avisos <br>
                                     Paroquiais:
                                 </h3>
-                                <a 
+                                <span 
                                 class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-regular u-font-family-lato text-center text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden py-2" 
                                 href="<?php echo get_home_url( null, '/fotos'); ?>">
                                    Ver agora
-                                </a>
+                                </span>
                             </div>
                         </a>
                     </div>
@@ -80,11 +80,11 @@
                                     Avisos <br>
                                     Paroquiais:
                                 </h3>
-                                <a 
+                                <span 
                                 class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-regular u-font-family-lato text-center text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden py-2" 
                                 href="<?php echo get_home_url( null, '/fotos'); ?>">
                                    Ver agora
-                                </a>
+                        </span>
                             </div>
                         </a>
                     </div>
